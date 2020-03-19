d05323070dcd24aae8369d08185d8a37452e5ad3
Derick J. Eisenhardt <derick.eisenhardt@gmail.com>
2020-03-18 20:15:15 -0400

    Added state manager, now loading data file

--- src/pages/dmail.jsx
+++ src/pages/dmail.jsx
@@ -3,18 +3,21 @@ import React from 'react';
 import './dmail.scss';
 import Navbar from '../components/navbar';
 import Sidebar from '../components/sidebar';
+import { DMailPageState } from '../state/dmailState';
 
 function DMail() {
   return (
     <div id='dmail'>
-      <Navbar />
-      <div className='flex-row'>
-        <Sidebar />
-        <div id='content-container'>
-          header
-          content
+      <DMailPageState>
+        <Navbar />
+        <div className='flex-row'>
+          <Sidebar />
+          <div id='content-container'>
+            header
+            content
+          </div>
         </div>
-      </div>
+      </DMailPageState>
     </div>
   )
 }
--- src/state/dmailState.jsx
+++ src/state/dmailState.jsx
@@ -1,1 +1,31 @@
-// TODO
+// I've opted for a simple state manager via React's Context system
+import React, { useEffect } from 'react';
+
+const SERVER = "http://localhost:3000"; // TODO: choose dynamically from environment
+const initialState = { currentEmail: null };
+
+export const DMailState = React.createContext();
+
+export function DMailPageState(props) {
+  const { children } = props;
+  const [state, setState] = React.useState(initialState);
+
+  useEffect(() => {
+    getEmails();
+  }, []);
+
+  return <DMailState.Provider value={state}>{children}</DMailState.Provider>
+
+  async function getEmails() {
+    const FILE_PATH = "/emails.json";
+    const emails = await fetch(SERVER + FILE_PATH)
+      .then(response => response.json())
+      .catch(error => console.error('Data file failed to load'));
+
+    setState(priorState => {
+      return { ...priorState, ...{ emailList: emails }};
+    });
+  }
+}
+
+export default DMailState;
