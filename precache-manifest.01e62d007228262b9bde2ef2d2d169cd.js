c7de28e80f478e79af07bdc14e9e2584a7eda72f
Derick J. Eisenhardt <derick.eisenhardt@gmail.com>
2020-03-26 14:29:14 -0400

    Updates

---
+++ precache-manifest.01e62d007228262b9bde2ef2d2d169cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313fa6bfa818ccc272beddd5a80b7ce1",
+    "url": "/dmail/index.html"
+  },
+  {
+    "revision": "1484554ab183bdb71f76",
+    "url": "/dmail/static/css/main.5b99f854.chunk.css"
+  },
+  {
+    "revision": "ffa2f4a3f3b794b1c129",
+    "url": "/dmail/static/js/2.62954d81.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/dmail/static/js/2.62954d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1484554ab183bdb71f76",
+    "url": "/dmail/static/js/main.4de51bec.chunk.js"
+  },
+  {
+    "revision": "a06ae7f17ddc8e8fb3bf",
+    "url": "/dmail/static/js/runtime-main.b6ea1dbf.js"
+  }
+]);
